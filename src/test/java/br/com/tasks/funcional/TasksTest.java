d1a586d8b5dba37441c70bed5f03bdfb81bbb2be
Hugosanches <hugosanches87@gmail.com>
2023-06-27 02:09:33 -0300

    comentar stege de teste

--- src/test/java/br/com/tasks/funcional/TasksTest.java
+++ src/test/java/br/com/tasks/funcional/TasksTest.java
@@ -1,24 +1,23 @@
 package br.com.tasks.funcional;
 
 import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TasksTest {
 
 	public WebDriver acessarAlicacao() throws MalformedURLException {
 		System.setProperty("webdriver.chrome.driver", "C:\\Desenvolvimento\\Driver\\chromedriver.exe");
-		// WebDriver driver = new ChromeDriver();
-		DesiredCapabilities cap = DesiredCapabilities.chrome();
-		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.23:4444/wd/hub"), cap);
-		driver.navigate().to("http://192.168.1.23:4444:8001/tasks");
+		WebDriver driver = new ChromeDriver();
+		//DesiredCapabilities cap = DesiredCapabilities.chrome();
+		//WebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.23:4444/wd/hub"), cap);
+		//driver.navigate().to("http://192.168.1.23:4444:8001/tasks");
+		driver.navigate().to("http://192.168.1.23:8001/tasks/");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
 	}
